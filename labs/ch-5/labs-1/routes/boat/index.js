22d773629d70e7776a1eceff880d70b7a73dc823
arturo.miguel.next <arturo.miguel.next@bbva.com>
2022-10-10 17:11:53 +0200

    chap 8

---
+++ labs/ch-5/labs-1/routes/boat/index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const { boat }  = require('../../model');
+
+module.exports = async (fastify, opts) => {
+  fastify.get('/:id', (request, reply) => {
+    const { id } = request.params
+    boat.read(id, (err, result) => {
+      if (err) {
+        if (err.message === 'not found') {
+          reply.notFound()
+        } else {
+          reply.send(err)
+        }
+      } else {
+        reply.send(result)
+      }
+    })
+  })
+}
--- my-service/model.js
+++ my-service/model.js
@@ -11,7 +11,25 @@ function bicycleModel () {
   }
 
   return {
-    read
+    create, read, update, del, uid
+  }
+
+  function uid () {
+    return Object.keys(db)
+      .sort((a, b) => a - b)
+      .map(Number)
+      .filter((n) => !isNaN(n))
+      .pop() + 1 + ''
+  }
+
+  function create (id, data, cb) {
+    if (db.hasOwnProperty(id)) {
+      const err = Error('resource exists')
+      setImmediate(() => cb(err))
+      return
+    }
+    db[id] = data
+    setImmediate(() => cb(null, id))
   }
 
   function read (id, cb) {
@@ -21,5 +39,25 @@ function bicycleModel () {
       return
     }
     setImmediate(() => cb(null, db[id]))
+  }
+
+  function update (id, data, cb) {
+    if (!(db.hasOwnProperty(id))) {
+      const err = Error('not found')
+      setImmediate(() => cb(err))
+      return
+    }
+    db[id] = data
+    setImmediate(() => cb())
+  }
+
+  function del (id, cb) {
+    if (!(db.hasOwnProperty(id))) {
+      const err = Error('not found')
+      setImmediate(() => cb(err))
+      return
+    }
+    delete db[id]
+    setImmediate(() => cb())
   }
 }
